9f31d787116234522bd79b534f9e01c7561e5b1c
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-08-14 14:30:45 -0300

    added process videos

--- AI-ML_Development/VideoFormaterClass.py
+++ AI-ML_Development/VideoFormaterClass.py
@@ -4,11 +4,19 @@ class VideoFormater:
     def __init__(self) -> None:
         pass
     
-    def formatVideo(self, points, numVideo):
-        dataFrame = pd.DataFrame({'puntos': points})
+    def formatVideo(self, points, translation, numVideo):
+        dataFrame = pd.DataFrame({'points': points, 'translation': translation})
         dataFrame['id'] = numVideo
         return dataFrame
     
     def concatAndExportVideos(self, videos):
         concatDf = pd.concat(videos, ignore_index=True)
         concatDf.to_csv("videosInfo.csv",index=False)
+
+    def csvToDfs(self, path):
+        df = pd.read_csv(path)
+        grouped = df.groupby('id')
+        df_list = []
+        for _, group in grouped:
+            df_list.append(group.reset_index(drop=True))
+        return df_list
