73a1434c0b6e2b7185db78e13a887fa544b8dcc6
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-08-15 16:10:08 -0300

    Modified process videos and formater class. Now working

--- AI-ML_Development/VideoFormaterClass.py
+++ AI-ML_Development/VideoFormaterClass.py
@@ -9,14 +9,20 @@ class VideoFormater:
         dataFrame['id'] = numVideo
         return dataFrame
     
-    def concatAndExportVideos(self, videos):
+    def concatAndExportVideos(self, videos, file_name):
         concatDf = pd.concat(videos, ignore_index=True)
-        concatDf.to_csv("videosInfo.csv",index=False)
+        concatDf.to_csv(file_name, index=False)
 
-    def csvToDfs(self, path):
+    def csvToDfsVid(self, path):
         df = pd.read_csv(path)
         grouped = df.groupby('id')
         df_list = []
         for _, group in grouped:
             df_list.append(group.reset_index(drop=True))
         return df_list
+
+    def csvToTranslationDf(self, path):
+        df = pd.read_csv(path, delimiter='\t')
+        df = df.drop(["VIDEO_ID", "SENTENCE_ID", "SENTENCE_NAME", "START", "END"], axis=1)
+        df = df.groupby('VIDEO_NAME')['SENTENCE'].apply(' '.join).reset_index()
+        return df
