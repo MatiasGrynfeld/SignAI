cb0453d85b67365e507e087e5c07ce6b4dd77184
MaxiSetton <maxisetton@gmail.com>
2024-08-22 15:55:24 -0300

    Update KeyFrameExtractorClass.py

--- AI-ML_Development/KeyFrameExtractorClass.py
+++ AI-ML_Development/KeyFrameExtractorClass.py
@@ -1,129 +1,7 @@
-# from PoseDetectorClass import PoseDetector
-# class KeyFrameExtractor:
-#     def __init__(self) -> None:
-#         self.poseDetector = PoseDetector(is_image=True, num_hands=2, detection_confidence=0.5, tracking_confidence=0.5)
-    
-#     def calcularDiferencia(self, puntos_previos, puntos_actuales, hands_detected_prev, hands_detected_actual):
-#         if puntos_previos is None:
-#             return 100.0
-#         if puntos_actuales is None:
-#             return 0.0
-#         if hands_detected_prev!=hands_detected_actual:
-#             return 100.0
-#         right_hand_prev = puntos_previos[:21] if hands_detected_prev >= 1 else None
-#         left_hand_prev = puntos_previos[21:42] if hands_detected_prev == 2 else None
-#         right_hand_actual = puntos_actuales[:21] if hands_detected_actual >= 1 else None
-#         left_hand_actual = puntos_actuales[21:42] if hands_detected_actual == 2 else None
-#         diff=self.calcularDifManos(right_hand_prev,right_hand_actual,left_hand_prev,left_hand_actual,hands_detected_actual)
-#         return float(diff)
-    
-#     def calcularDifManos(self, right_hand_prev, right_hand_actual, left_hand_prev, left_hand_actual, num_hands):
-#         total_diff = 0.0
-        
-#         def calc_diff(hand_prev, hand_actual):
-#             return sum([
-#                 abs(lm1.x - lm2.x) + abs(lm1.y - lm2.y) + abs(lm1.z - lm2.z)
-#                 for lm1, lm2 in zip(hand_prev, hand_actual)
-#             ])
-        
-#         if num_hands == 2:
-#             if right_hand_prev and right_hand_actual:
-#                 total_diff += calc_diff(right_hand_prev, right_hand_actual)
-#             if left_hand_prev and left_hand_actual:
-#                 total_diff += calc_diff(left_hand_prev, left_hand_actual)
-#             total_diff /= 2
-            
-#         elif num_hands == 1:
-#             if left_hand_prev and left_hand_actual:
-#                 total_diff = calc_diff(left_hand_prev, left_hand_actual)
-#             elif right_hand_prev and right_hand_actual:
-#                 total_diff = calc_diff(right_hand_prev, right_hand_actual)
-#             else:
-#                 return 100.0
-        
-#         return total_diff
 
-#     def extractKeyFrames(self, return_frame, draw, video, min_frame_interval=1):
-#         puntos_previos = []
-#         manos_detectadas_actual=0
-#         key_frames = []
-#         frame_count = 0
-#         ret = 1
-#         while ret:
-#             ret, frame = video.read()
-#             if not ret:
-#                 break
-#             frame_count += 1
-#             if len(key_frames)==0:
-#                 results = self.poseDetector.extractPoints(frame)
-#                 if results[0].multi_hand_landmarks:
-#                     puntos_previos = results[0].multi_hand_landmarks
-#                     manos_detectadas_prev = len(results[0].multi_hand_landmarks) // 21
-#                     threshold = self.adjust_threshold(puntos_previos)
-#                     if return_frame:
-#                         if draw:
-#                             frame = self.poseDetector.drawLandmarks(puntos_previos, frame)
-#                         key_frames.append((frame, frame_count))
-#                     else:
-#                         key_frames.append((results, frame_count))
-
-#             elif frame_count - key_frames[-1][1] > min_frame_interval:
-#                 results = self.poseDetector.extractPoints(frame)
-#                 if results[0].multi_hand_landmarks:
-#                     puntos_actuales = results[0].multi_hand_landmarks
-#                     manos_detectadas_actual = len(results[0].multi_hand_landmarks) // 21
-#                     diff = self.calcularDiferencia(puntos_previos, puntos_actuales, manos_detectadas_prev, manos_detectadas_actual)
-
-#                     if diff > threshold:
-#                         if return_frame:
-#                             if draw:
-#                                 frame = self.poseDetector.drawLandmarks(puntos_actuales, frame)
-#                             key_frames.append((frame, frame_count))
-#                         else:
-#                             key_frames.append((results, frame_count))
-#                             puntos_previos = puntos_actuales
-#                             manos_detectadas_prev = manos_detectadas_actual
-#                 else:
-#                     puntos_previos = None
-#         video.release()
-#         return [frame for frame, _ in key_frames]
-
-#     def adjust_threshold(self, puntos, base_threshold=2.0):
-#         y_values = []
-#         z_values=[]
-#         x_values=[]
-#         ysum=0
-#         xsum=0
-#         zsum=0
-#         if puntos[:21]:
-#             for z in puntos[:21]:
-#                 ysum += z.y
-#                 xsum += z.x
-#                 zsum += z.z
-#             y_values.append(ysum / len(puntos[:21]))
-#             x_values.append(xsum / len(puntos[:21]))
-#             z_values.append(zsum / len(puntos[:21]))
-            
-#         if puntos[21:42]:
-#             for z in puntos[21:42]:
-#                 ysum += z.y
-#                 xsum += z.x
-#                 zsum += z.z
-#             y_values.append(ysum / len(puntos[21:42]))
-#             x_values.append(xsum / len(puntos[21:42]))
-#             z_values.append(zsum / len(puntos[21:42]))
-        
-#         if not z_values:
-#             return base_threshold
-        
-#         average_y = sum(y_values) / len(y_values)
-#         average_x = sum(x_values) / len(x_values)
-#         average_z = sum(z_values) / len(z_values)
-#         adjusted_threshold = base_threshold + abs(average_z * 50 * base_threshold)
-#         print(adjusted_threshold)
-#         return adjusted_threshold
-
+from colorama import Fore, Style
 from PoseDetectorClass import PoseDetector
+import numpy as np
 class KeyFrameExtractor:
     def __init__(self) -> None:
         self.poseDetector = PoseDetector(is_image=True, num_hands=2, detection_confidence=0.5, tracking_confidence=0.5)
@@ -153,6 +31,7 @@ class KeyFrameExtractor:
                 for lm1, lm2 in zip(hand_prev, hand_actual)
             ])
             total_diff += diff
+        total_diff/=21
         if num_hands_previas >= 2:
             total_diff /= num_hands_previas
         return float(total_diff)
@@ -171,6 +50,8 @@ class KeyFrameExtractor:
         handedness_prev = []
         key_frames = []
         frame_count = 0
+        differencia=[]
+        thresholdadjusted=[]
 
         for frame in self.extractFrames(video):
             frame_count += 1
@@ -194,7 +75,9 @@ class KeyFrameExtractor:
                     puntos_actuales = results[0].multi_hand_landmarks
                     handedness_actual = results[0].multi_handedness if results[0].multi_handedness else []
                     diff = self.calcularDiferencia(puntos_previos, puntos_actuales, handedness_prev, handedness_actual)
-
+                    differencia.append(diff)
+                    th=self.adjust_threshold(puntos_actuales)
+                    thresholdadjusted.append((diff, th))
                     if diff > threshold:
                         if return_frame:
                             if draw:
@@ -206,14 +89,27 @@ class KeyFrameExtractor:
                         handedness_prev = handedness_actual
                 else:
                     puntos_previos = None
+        for a in thresholdadjusted:
+            print(f"{Fore.GREEN}{a}{Style.RESET_ALL}\n")
         return [frame for frame, _ in key_frames]
     
-    def adjust_threshold(self, puntos_actuales, base_threshold=40.0):
+    def adjust_threshold(self, puntos_actuales, min_threshold=0.075, max_threshold=0.4):
+        hands_size=[]
+        # z_values=[]
+        # for handlandmarks in puntos_actuales.multi_hand_world_landmarks:
+        #     z_values.extend(landmark.z for landmark in handlandmarks.landmark)
+        # z_prom=sum(z_values)/len(z_values)
+        # threshold= base_threshold+base_threshold/z_prom
         for hand_landmarks in puntos_actuales:
             x_coords = [landmark.x for landmark in hand_landmarks.landmark]
             y_coords = [landmark.y for landmark in hand_landmarks.landmark]
-        hand_width = max(x_coords) - min(x_coords)
-        hand_height = max(y_coords) - min(y_coords)
-        hands_size= hand_width * hand_height
-        threshold=base_threshold*hands_size
+            hand_width = max(x_coords) - min(x_coords)
+            hand_height = max(y_coords) - min(y_coords)
+            hands_size.append(hand_width * hand_height)
+        if hands_size:
+            hands_media_size=sum(hands_size)/len(puntos_actuales)
+        mid_size = 0.25  # Puedes ajustar este valor
+        k = 7.5  # Ajusta para cambiar la sensibilidad
+        threshold = min_threshold + (max_threshold - min_threshold) / (1 + np.exp(-k * (hands_media_size - mid_size)))
+        print(Fore.GREEN+f'Threshold: ${threshold} \n Hands size: ${hands_media_size}'+Style.RESET_ALL)
         return threshold
