b4e66c67dc4ce49fd4ec9d85ad1b94bf2b6d033f
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-08-26 15:53:09 -0300

    ouput txt para después modificar point2vec

--- AI-ML_Development/PoseDetectorClass.py
+++ AI-ML_Development/PoseDetectorClass.py
@@ -57,7 +57,19 @@ if __name__ == '__main__':
     image = cv2.imread("C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources\\a.jpg")
     pd = PoseDetector(is_image=True, num_hands=2, detection_confidence=0.75, tracking_confidence=0.75)
     r = pd.extractPoints(frame=image)
-    image = pd.drawLandmarks(results=r, frame=image)
-    cv2.imshow("Image", image)
+    new_image = pd.drawLandmarks(results=r, frame=image)
+    cv2.imshow("Image", new_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    r_left_hand = r.left_hand_landmarks
+    r_right_hand = r.right_hand_landmarks
+    r_pose = r.pose_landmarks
+    r_face = r.face_landmarks
+    with open("output.txt", "w") as file:
+        file.write(str(r_left_hand) + "\n")
+        file.write("enter\n")
+        file.write(str(r_right_hand) + "\n")
+        file.write("enter\n")
+        file.write(str(r_pose) + "\n")
+        file.write("enter\n")
+        file.write(str(r_face) + "\n")
