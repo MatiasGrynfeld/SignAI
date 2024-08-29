db5a52d7f5492594a84ee6c390c1bf5ae850af5e
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-08-29 15:56:43 -0300

    updated to test 2 large videos

--- AI-ML_Development/Tests/pruebaKeyFrameExtractor.py
+++ AI-ML_Development/Tests/pruebaKeyFrameExtractor.py
@@ -5,15 +5,23 @@ init()
 from KeyFrameExtractorClass import KeyFrameExtractor
 from Points2VecClass import Point2Vec
 
-video = cv2.VideoCapture('C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources\\Videos\\Test\\_2FBDaOPYig-5-rgb_front.mp4')
+#video = cv2.VideoCapture('C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources\\Videos\\5RR7dlzkyL0-5-rgb_front.mp4')
+video2 = cv2.VideoCapture('C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources\\Videos\\a5yNwUSiYpA-3-rgb_front.mp4')
 KFE = KeyFrameExtractor()
 
-keyFrames = KFE.extractKeyFrames(return_frame=False, draw=False, video=video)
-p2v = Point2Vec(4)
-keyFrames = p2v.land2vec(keyFrames)
-print(len(keyFrames))
-print(keyFrames)
+#keyFrames = KFE.extractKeyFrames(return_frame=True, draw=True, video=video)
+print("segundo video")
+keyFrames2 = KFE.extractKeyFrames(return_frame=True, draw=True, video=video2)
+# p2v = Point2Vec(4)
+# keyFrames = p2v.land2vec(keyFrames)
+#print(len(keyFrames))
+# print(keyFrames)
 # for frame in keyFrames:
 #     cv2.imshow('frame', frame)
 #     cv2.waitKey(0)
 #     cv2.destroyAllWindows()
+print(len(keyFrames2))
+for frame in keyFrames2:
+    cv2.imshow('frame', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
