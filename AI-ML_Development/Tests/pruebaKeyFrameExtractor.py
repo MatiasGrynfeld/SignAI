1286a69aff557eaabaf71c80553b27a9025718d7
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-09-02 14:31:25 -0300

    testeado key frame extractor

--- AI-ML_Development/Tests/pruebaKeyFrameExtractor.py
+++ AI-ML_Development/Tests/pruebaKeyFrameExtractor.py
@@ -1,27 +1,30 @@
 import cv2
 from forAllTest import init
+import os
 
 init()
 from KeyFrameExtractorClass import KeyFrameExtractor
 from Points2VecClass import Point2Vec
 
-#video = cv2.VideoCapture('C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources\\Videos\\5RR7dlzkyL0-5-rgb_front.mp4')
-video = cv2.VideoCapture('C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources\\_2FBDaOPYig-5-rgb_front.mp4')
-KFE = KeyFrameExtractor()
+def obtain_paths(root, extension):
+    paths = []
 
-keyFrames = KFE.extractKeyFrames(return_frame=True, draw=True, video=video)
-# print("segundo video")
-# keyFrames2 = KFE.extractKeyFrames(return_frame=True, draw=True, video=video2)
-# p2v = Point2Vec(4)
-# keyFrames = p2v.land2vec(keyFrames)
-# print(len(keyFrames))
-# print(keyFrames)
-for frame in keyFrames:
-    cv2.imshow('frame', frame)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-# print(len(keyFrames2))
-# for frame in keyFrames2:
-#     cv2.imshow('frame', frame)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+    for filename in os.listdir(root):
+        if filename.endswith(extension):
+            paths.append(os.path.join(root, filename))
+    return paths
+
+vids = obtain_paths("C:\\Users\\48519558\\Desktop\\SignAI-ML\\AI-ML_Development\\Resources", '.mp4')
+print(vids)
+
+kfe = KeyFrameExtractor()
+p2v = Point2Vec()
+
+for video in vids:
+    vid = cv2.VideoCapture(video)
+    frames = kfe.extractKeyFrames(return_frame=True, draw=True, video=vid)
+    print(len(frames))
+    for frame in frames:
+        cv2.imshow('frame', frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
