6e47f157987a7b4524dd43f1059a0445262e46d1
MatiasGrynfeld <48519558@est.ort.edu.ar>
2024-09-10 18:55:14 -0300

    added upload video to api tests

---
+++ API/Tests/uploadVideo.py
@@ -0,0 +1,24 @@
+import cloudinary
+import os
+
+cloud_name = os.getenv('CLOUD_NAME')
+api_key = os.getenv('API_KEY')
+api_secret = os.getenv('API_SECRET')
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = cloud_name, 
+    api_key = api_key, 
+    api_secret = api_secret,
+    secure=True
+)
+
+import cloudinary.uploader
+
+# Upload an image
+upload_result = cloudinary.uploader.upload(
+    "D:\\SignAI-ML\\AI-Module\\Resources\\Videos\\_2FBDaOPYig-5-rgb_front.mp4", 
+    public_id="video_prueba",
+    resource_type = "video"
+)
+print(upload_result["secure_url"])
