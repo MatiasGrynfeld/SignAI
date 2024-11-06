08c205dec663f9af789a97ec5d34c9f8375563da
MatiasGrynfeld <141443983+MatiasGrynfeld@users.noreply.github.com>
2024-11-06 14:25:00 -0300

    agrego cors

--- API/init.py
+++ API/init.py
@@ -1,27 +1,40 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from Controllers.root import api_working
 from Controllers.translate import post_translate
 from Body.translate import BodyTranslate
+from pydantic import BaseModel
 
 app = FastAPI()
 port = 8000
 
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas las solicitudes de cualquier origen
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos los headers
+)
+
+# Endpoints
 @app.get("/")
 async def root():
     return api_working()
 
 @app.post("/translate")
 async def translate(body: BodyTranslate):
+    print("llego algo")
     translation = await post_translate(body)
     return translation
 
-from pydantic import BaseModel
 class info(BaseModel):
     id: int
     translation: str
+
 @app.post("/prueba")
 async def prueba(body: info):
     print(body)
     return {"received": body}
 
-#uvicorn init:app --host 0.0.0.0 --port 8000 --reload --> to run the server
+# Para correr el servidor: uvicorn init:app --host 0.0.0.0 --port 8000 --reload
